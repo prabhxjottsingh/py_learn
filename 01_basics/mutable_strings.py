d5fe2282ed62cddf0a970c98d974850b2afcc84c
Prabhjot Singh <prabhx.jott@gmail.com>
2025-04-27 09:53:49 +0530

    added mutable and immutable strings basics

---
+++ 01_basics/mutable_strings.py
@@ -0,0 +1,12 @@
+username = "prabhjot"
+print("username: " + username)
+
+username = "prabhjot singh"
+print("username: " + username)
+
+username_second = username
+print("username_second: " + username_second)
+
+username = "prabhjot singh who?"
+print("username: " + username)
+print("username_second " + username_second)
